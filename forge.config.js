2f2c2f371a1cc1045534947037d7c4f39ffaeea7
Martin Bissiger <martin.bissiger@students.bfh.ch>
2026-02-17 23:56:48 +0100

    push

---
+++ forge.config.js
@@ -0,0 +1,56 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+
+module.exports = {
+  packagerConfig: {
+    asar: true,
+    osxSign: {
+      identity: 'Developer ID Application: Aarenet AG (HN497WV2TX)',
+      'hardened-runtime': true,
+      entitlements: 'assets/entitlements.mac.plist',
+      'entitlements-inherit': 'assets/entitlements.plist',
+      'signature-flags': 'library',
+    },
+    osxNotarize: {
+      appleId: 'apple@aarenet.com',
+      appleIdPassword: 'vhzw-hfwu-cypj-wkdv',
+      teamId: 'HN497WV2TX',
+    },
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
+};
--- src/renderer/app/utils/firebase.js
+++ src/renderer/app/utils/firebase.js
@@ -11,19 +11,20 @@ import {
   addDoc,
   doc,
 } from 'firebase/firestore';
+
 import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-/*const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};*/
+const firebaseConfig = {
+  apiKey: 'AIzaSyDdrtbtQI39qtARGZqw-x0wTmkw9ra5qYo',
+  authDomain: 'chatapp-2e4fa.firebaseapp.com',
+  projectId: 'chatapp-2e4fa',
+  storageBucket: 'chatapp-2e4fa.firebasestorage.app',
+  messagingSenderId: '118520688534',
+  appId: '1:118520688534:web:4c83ee97c859b03ff137b7',
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
